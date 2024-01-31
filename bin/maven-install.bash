f901185a09a3d1b91c1c5192d0a1e4636677f1ce
digital-hot <grey.egorov@gmail.com>
2024-01-31 16:42:59 +0200

    Initial commit

---
+++ bin/maven-install.bash
@@ -0,0 +1,1 @@
+mvn install:install-file -Dfile=/Volumes/Data/java/project-maven-master/lib/desktop-game-engine.jar -DgroupId=com.javarush -DartifactId=desktop-game-engine -Dversion=1.0 -Dpackaging=jar
